272c36a56ff70cec3a81a844a5594c3d9c2ba395
Avinash Jeyasankar <avinashj@CESCHN.COM>
2017-10-10 21:44:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div class="container">
+  <div *ngIf="afAuth.authState | async; let user; else showLogin" class="card align-self-center" style="width: 20rem;">
+  <img class="card-img-top rounded-circle" [src]="user.photoURL" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title"> {{ user.displayName}}</h4>
+    <div class="alert alert-success" role="alert">
+    Succesfully logged in!
+  </div>
+    <button (click)="logout()">Logout</button>
+  </div>
+  </div>
+</div>
+  
+    <ng-template #showLogin>
+      <p>Please login.</p>
+      <button (click)="login()">Login with Google</button>
+      <button (click)="facebookLogin()">Login with Facebook</button>
+    </ng-template>
+  `,
+  styleUrls: ['./app.component.css'],
+  providers: [AngularFireAuth]
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(public afAuth: AngularFireAuth) {
+  }
+  login() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+    this.afAuth.auth.signInWithPopup(provider).then(function(result) {
+     // This gives you a Google Access Token.
+     var token = result.credential.accessToken;
+     console.log(token);
+     // The signed-in user info.
+     var user = result.user;
+     console.log(user);
+    });
+  }
+  facebookLogin() {
+    var provider = new firebase.auth.FacebookAuthProvider();;
+    provider.addScope('user_birthday');
+    provider.addScope('email');
+    this.afAuth.auth.signInWithPopup(provider).then(function(result) {
+     // This gives you a Google Access Token.
+     var token = result.credential.accessToken;
+     console.log(token);
+     // The signed-in user info.
+     var user = result.user;
+     console.log(user);
+    });
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from 'angularfire2';
+
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyCRv69DsqAEE5SwnSqouXf_DjS4YLNzDFk',
+      authDomain: 'fir-app-7cb0a.firebaseapp.com',
+      databaseURL: 'https://fir-app-7cb0a.firebaseio.com',
+      projectId: 'fir-app-7cb0a',
+      storageBucket: 'fir-app-7cb0a.appspot.com',
+      messagingSenderId: '619419098799'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
