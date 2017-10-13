6868090e097a2c3d3cdd8f9ea1bc9fbfe79e0296
Avinash Jeyasankar <avinashj@CESCHN.COM>
2017-10-13 18:26:48 +0530

    made arrow function change for this keyword not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,55 +5,27 @@ import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <div class="container">
-  <div *ngIf="afAuth.authState | async; let user; else showLogin" class="card align-self-center" style="width: 20rem;">
-  <img class="card-img-top rounded-circle" [src]="user.photoURL" alt="Card image cap">
-  <div class="card-body">
-    <h4 class="card-title"> {{ user.displayName}}</h4>
-    <div class="alert alert-success" role="alert">
-    Succesfully logged in!
-  </div>
-    <button type="button" class="btn btn-warning" (click)="logout()">Logout</button>
-  </div>
-  </div>
-</div>
-  
-    <ng-template #showLogin>
-
-
-<div class="jumbotron">
-  <h1 class="display-3">Please login.</h1>
-  <p class="lead">This is a simple angular firebase app to test authentication.</p>
-  <hr class="my-4">
-  <p class="lead">
-       <button type="button" class="btn btn-sm btn-social btn-lg btn-google" (click)="login()">
-     <i class="fa fa-google"></i>Sign in with Google</button>
-      <button type="button" class="btn btn-sm btn-social btn-lg btn-facebook" (click)="facebookLogin()">
-      <i class="fa fa-facebook"></i>Sign in with Facebook</button>
-  </p>
-</div>
-    </ng-template>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [AngularFireAuth]
 })
 export class AppComponent {
-  facebook='facebook';
+ public facebook:boolean=false;
   constructor(public afAuth: AngularFireAuth) {
   }
   login() {
     var provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('profile');
     provider.addScope('email');
-    this.afAuth.auth.signInWithPopup(provider).then(function(result) {
+    this.afAuth.auth.signInWithPopup(provider).then( (result) => {
      // This gives you a Google Access Token.
      var token = result.credential.accessToken;
      console.log(token);
      // The signed-in user info.
      var user = result.user;
      console.log(user);
-     this.facebook = 'google';
+     this.facebook = false;
+     console.log(this.facebook);
     });
   }
   facebookLogin() {
@@ -62,14 +34,16 @@ export class AppComponent {
     provider.addScope('email');
     provider.addScope('public_profile');
     provider.addScope('user_friends');
-    this.afAuth.auth.signInWithPopup(provider).then(function(result) {
+    this.afAuth.auth.signInWithPopup(provider).then( (result) => {
      // This gives you a Google Access Token.
      var token = result.credential.accessToken;
      console.log(token);
      // The signed-in user info.
      var user = result.user;
      console.log(user);
-     this.facebook='facebook';
+     this.facebook=true;
+     console.log(this.facebook);
+      
     });
   }
   logout() {
